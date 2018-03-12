97c23567627d56fd2d80d34c659bc6a1732a1d04
Unknown <mstatt@stanford.edu>
2018-03-12 15:16:35 -0700

    Automating AWS login and termiantion

---
+++ fast_ai/connect.sh
@@ -0,0 +1,11 @@
+
+#Connect to instances
+export name=fast-ai
+export key_name=aws-key-$name
+
+export ip_address=`aws ec2 describe-instances --filters Name=tag:Name,Values=$name-gpu-machine --filters Name=instance-state-name,Values=running --output text --query 'Reservations[*].Instances[0].PublicIpAddress'`
+export instance_id=`aws ec2 describe-instances --filters Name=tag:Name,Values=$name-gpu-machine --filters Name=instance-state-name,Values=running --output text --query 'Reservations[*].Instances[0].InstanceId'`
+echo IP Address of instance: $instance_id
+echo connecting to IP address: $ip_address
+#Connect to instance
+ssh -i ~/.ssh/aws-key-$name.pem ubuntu@$ip_address
--- fast_ai/start_spot.sh
+++ fast_ai/start_spot.sh
@@ -1,4 +1,7 @@
 # The config file was created in ondemand_to_spot.sh
+export name=fast-ai
+export key_name=aws-key-$name
+
 export config_file=my.conf
 while [[ $# -gt 0 ]]
 do
@@ -24,14 +27,17 @@ export request_id=`../ec2spotter-launch $config_file`
 echo Spot request ID: $request_id
 
 echo Waiting for spot request to be fulfilled...
-aws ec2 wait spot-instance-request-fulfilled --spot-instance-request-ids $request_id  
+aws ec2 wait spot-instance-request-fulfilled --spot-instance-request-ids $request_id
 
 export instance_id=`aws ec2 describe-spot-instance-requests --spot-instance-request-ids $request_id --query="SpotInstanceRequests[*].InstanceId" --output="text"`
 
 echo Waiting for spot instance to start up...
 aws ec2 wait instance-running --instance-ids $instance_id
 
-echo Spot instance ID: $instance_id 
+# Change the instance name
+aws ec2 create-tags --resources $instance_id --tags --tags Key=Name,Value=$name-gpu-machine
+
+echo Spot instance ID: $instance_id
 
 echo 'Please allow the root volume swap script a few minutes to finish.'
 if [ "x$ec2spotter_elastic_ip" = "x" ]
@@ -41,10 +47,10 @@ then
 else
 	# Elastic IP
 	export ip=`aws ec2 describe-addresses --allocation-ids $ec2spotter_elastic_ip --output text --query 'Addresses[0].PublicIp'`
-fi	
+fi
 
 export name=fast-ai
-if [ "$ec2spotter_key_name" = "aws-key-$name" ] 
+if [ "$ec2spotter_key_name" = "aws-key-$name" ]
 then
 	echo Then connect to your instance: ssh -i ~/.ssh/aws-key-$name.pem ubuntu@$ip
 fi
